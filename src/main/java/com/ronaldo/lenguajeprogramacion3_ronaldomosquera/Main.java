f211b3be3d4ab5c235a2f64be7879aa3ba75e0df
Diego F Marin <dfmarin@stio.com>
2026-05-21 23:44:26 -0500

    Primer proyecto LP3

---
+++ src/main/java/com/ronaldo/lenguajeprogramacion3_ronaldomosquera/Main.java
@@ -0,0 +1,11 @@
+package com.ronaldo.lenguajeprogramacion3_ronaldomosquera;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hola, Lenguaje de Programación 3");
+
+    }
+
+}
